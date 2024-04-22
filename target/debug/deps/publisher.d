46ddc86cd393a2673a88f7b1f9b9af9faa400d86
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-04-22 23:53:54 +0700

    Initial publisher code.

---
+++ target/debug/deps/publisher.d
@@ -0,0 +1,5 @@
+C:\Users\ASUSTeK\Documents\Fasilkom\SEM 4\Adpro\Tutorial 8\publisher\target\debug\deps\publisher.exe: src\main.rs
+
+C:\Users\ASUSTeK\Documents\Fasilkom\SEM 4\Adpro\Tutorial 8\publisher\target\debug\deps\publisher.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/publisher.d
@@ -0,0 +1,1 @@
+C:\Users\ASUSTeK\Documents\Fasilkom\SEM\ 4\Adpro\Tutorial\ 8\publisher\target\debug\publisher.exe: C:\Users\ASUSTeK\Documents\Fasilkom\SEM\ 4\Adpro\Tutorial\ 8\publisher\src\main.rs
